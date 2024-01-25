d3ac43d180b1518ead10803822a24cd7ff4f4959
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2024-01-25 16:41:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstChar(text) {
-  // your code here
+  
+ 
+  text=text.trim()
+  if (text.length==0) 
+    return ""
+    return text.charAt(0)
+  
 }
 
-// Do not change the code below
+
 
 const text = prompt("Enter text:");
 alert(firstChar(text));
